1915a28c5fb00316bac9f1bcd2f959d0bb1c6a00
samuelhenrique1234-ops <samuel_moraes@estudante.sesisenai.org.br>
2025-07-07 11:55:05 -0300

    ADD - Questão 3

--- index2.php
+++ index2.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Questão 1</title>
+    <title>Questão 2</title>
 </head>
 
 <body>
 
     <form method="POST" action="">
-        <label for="numero_tabuada">Calcular o tabuada do número:</label>
+        <label for="numero_tabuada">Calcular a tabuada do número:</label>
         <input type="number" id="numero_tabuada" name="numero_tabuada" required>
         <button type="submit" name="verificar_tabuada">Verificar</button>
 
