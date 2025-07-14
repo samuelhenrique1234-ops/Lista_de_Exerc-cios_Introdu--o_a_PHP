8212987353c980352bb21c9715fed1074038ee0e
samuelhenrique1234-ops <samuel_moraes@estudante.sesisenai.org.br>
2025-07-14 08:45:08 -0300

    ADD - Questão 6

---
+++ index6.php
@@ -0,0 +1,33 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questão 3</title>
+</head>
+
+<body>
+
+    <form method="POST" action="">
+        <label for="number">Informe o número:</label>
+        <input type="number" id="number" name="number" required>
+        <button type="submit" name="fatorial">Verificar</button>
+    </form>
+
+    <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        if (isset($_POST['exibir_divisores'])) {
+            $numero = $_POST['numero'];
+            for($i = 1; $i <= $numero; $i++){
+                if($numero % $i == 0){
+                    echo $i . " ";
+                    };
+                };
+            };
+        };
+    ?>
+
+</body>
+
+</html>
